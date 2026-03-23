1890a8623a7710cc62af6e1b222d4bc8aebe6a29
Bat004 <b23173246@gmail.com>
2026-03-23 16:50:25 +0100

    e

--- .next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"+wZNldiFZhioZQ2LSkEDZoMsf9lowJj0htoyLcwEVW8=\"\n}"
--- .next/dev/static/chunks/WebstormProjects_Bar-papa_app_layout_tsx_0aktatj._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/[root-of-the-server]__0h1n875._.css","static/chunks/0g9b_next_dist_0rvjccw._.js"],
-    source: "dynamic"
-});

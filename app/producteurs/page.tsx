5310524941c9a52acaeeae253a58bced6863cfb7
Bat004 <b23173246@gmail.com>
2026-03-24 14:41:17 +0100

    pages produits et producteurs add

---
+++ app/producteurs/page.tsx
@@ -0,0 +1,13 @@
+
+export default function Producteurs() {
+    return (
+        <div className="flex flex-col min-h-screen bg-zinc-300 text-zinc-950 font-sans">
+            <header className="py-12 px-6 text-center">
+                <h1 className="text-5xl font-bold tracking-tight">
+                    Producteurs
+                </h1>
+            </header>
+        </div>
+    );
+}
+

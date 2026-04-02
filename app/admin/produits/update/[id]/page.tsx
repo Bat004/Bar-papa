a00a62597335bfb1b77e9d062fa6560119d2f55e
Remy25580 <remy.martinot@coda-student.school>
2026-04-02 09:55:08 +0200

    feat: les formulaires pour les produits sont faits

--- app/admin/produits/update/[id]/page.tsx
+++ app/admin/produits/update/[id]/page.tsx
@@ -4,46 +4,56 @@ import React, { useState, useEffect, use } from 'react';
 import { useRouter } from 'next/navigation';
 import Link from "next/link";
 
-export default function UpdateProducteur({ params }: { params: Promise<{ id: string }> }){
+export default function UpdateProduit({ params }: { params: Promise<{ id: string }> }){
     const resolvedParams = use(params); 
     const id = resolvedParams.id;
-    
-    const [fullName, setName] = useState('');
+
+    const [name, setName] = useState('');
     const [description, setDescription] = useState('');
-    const [region, setRegion] = useState('');
+    const [type, setType] = useState('');
+    const [shopLink, setLink] = useState('');
+    const [idproducteur, setIdProducteur] = useState('');
     const [error, setError] = useState(false);
     const router = useRouter();
-    
+
     useEffect(() => {
-    const fetchProducteur = async () => {
-        if (!id) return;
-        try {
-            const res = await fetch(`/api/admin/producteurs/${id}`); 
-            
-            if (res.ok) {
-                const data = await res.json();
-                setName(data.nom || ''); 
-                setDescription(data.description || '');
-                setRegion(data.region?.nom || '');
+        const fetchProduit = async () => {
+            if (!id) return;
+            try {
+                const res = await fetch(`/api/admin/produits/${id}`); 
+                
+                if (res.ok) {
+                    const data = await res.json();
+                    setName(data.nom || '');
+                    setDescription(data.description || '');
+                    setType(data.type || '');
+                    setLink(data.lienBoutique || '');
+                    setIdProducteur(data.producteurId || '');
+                }
+            } catch (err) {
+                console.error("Erreur lors du chargement :", err);
             }
-        } catch (err) {
-            console.error("Erreur lors du chargement :", err);
-        }
-    };
-    fetchProducteur();
-}, [id]);
+        };
+        fetchProduit();
+    }, [id]);
 
     const updateSubmit = async (e: React.FormEvent) => {
         e.preventDefault();
         setError(false);
-
+    
         try{
-            const res = await fetch(`/api/admin/producteurs/${id}`, {
+            const res = await fetch(`/api/admin/produits/${id}`, {
                 method: 'PATCH',
                 headers: { 'Content-Type': 'application/json' },
-                body: JSON.stringify({nom : fullName, description, regionId :  region})
+                body: JSON.stringify({
+                    nom: name,
+                    description: description,
+                    type: type,
+                    lienBoutique: shopLink, 
+                    producteurId: idproducteur
+                })
             });
-
+    
             if(res.ok){
                 router.push('/admin/dashboard')
                 router.refresh();
@@ -52,7 +62,7 @@ export default function UpdateProducteur({ params }: { params: Promise<{ id: str
                 setError(true);
             }
         }catch(error){
-            console.error("Erreur dans la modification du producteur : ", error);
+            console.error("Erreur dans la modification du produit : ", error);
             setError(true);
         }
     };
@@ -60,13 +70,13 @@ export default function UpdateProducteur({ params }: { params: Promise<{ id: str
     const deletionSubmit = async (e: React.FormEvent) => {
         e.preventDefault();
         setError(false);
-
+    
         try{
-            const res = await fetch(`/api/admin/producteurs/${id}`, {
+            const res = await fetch(`/api/admin/produits/${id}`, {
                 method: 'DELETE',
                 headers: { 'Content-Type': 'application/json' },
             });
-
+    
             if(res.ok){
                 router.push('/admin/dashboard')
                 router.refresh();
@@ -75,17 +85,17 @@ export default function UpdateProducteur({ params }: { params: Promise<{ id: str
                 setError(true);
             }
         }catch(error){
-            console.error("Erreur dans la supression du producteur : ", error);
+            console.error("Erreur dans la supression du produit : ", error);
             setError(true);
         }
     }
 
     return (
         <div className="flex flex-col min-h-screen bg-zinc-300 text-zinc-950 items-center p-8 sm:p-12">
-
+            
             <div className="w-full max-w-md">
                 <div className="mb-10 text-center">
-                    <h1 className="text-2xl font-bold uppercase tracking-widest">Modifier ce producteur</h1>
+                    <h1 className="text-2xl font-bold uppercase tracking-widest">Modifier ce produit</h1>
                 </div>
 
                 <form onSubmit={updateSubmit} className="flex flex-col gap-6">
@@ -100,20 +110,8 @@ export default function UpdateProducteur({ params }: { params: Promise<{ id: str
                         <input
                             type="text"
                             placeholder="entrez un nom"
-                            value={fullName}
+                            value={name}
                             onChange={(e) => setName(e.target.value)}
-                            className="border border-zinc-950 bg-transparent px-4 py-2 outline-none focus:bg-zinc-200 transition-colors placeholder:text-zinc-500"
-                            required
-                        />
-                    </div>
-
-                    <div className="flex flex-col gap-2">
-                        <label className="text-xs font-bold uppercase tracking-tighter">Région</label>
-                        <input
-                            type="text"
-                            placeholder="entrez une région"
-                            value={region}
-                            onChange={(e) => setRegion(e.target.value)}
                             className="border border-zinc-950 bg-transparent px-4 py-2 outline-none focus:bg-zinc-200 transition-colors placeholder:text-zinc-500"
                             required
                         />
@@ -126,6 +124,43 @@ export default function UpdateProducteur({ params }: { params: Promise<{ id: str
                             value={description}
                             onChange={(e) => setDescription(e.target.value)}
                             className="border border-zinc-950 bg-transparent px-4 py-2 outline-none focus:bg-zinc-200 transition-colors placeholder:text-zinc-500 min-h-[100px] resize-none"
+                            required
+                        />
+                    </div>
+
+                    <div className="flex flex-col gap-2">
+                        <label className="text-xs font-bold uppercase tracking-tighter">Type d'alcool</label>
+                        <input
+                            type="text"
+                            placeholder="entrez une région"
+                            value={type}
+                            onChange={(e) => setType(e.target.value)}
+                            className="border border-zinc-950 bg-transparent px-4 py-2 outline-none focus:bg-zinc-200 transition-colors placeholder:text-zinc-500"
+                            required
+                        />
+                    </div>
+
+                    <div className="flex flex-col gap-2">
+                        <label className="text-xs font-bold uppercase tracking-tighter">Lien vers la boutique</label>
+                        <input
+                            type="text"
+                            placeholder="entrez le lien du produit"
+                            value={shopLink}
+                            onChange={(e) => setLink(e.target.value)}
+                            className="border border-zinc-950 bg-transparent px-4 py-2 outline-none focus:bg-zinc-200 transition-colors placeholder:text-zinc-500"
+                            required
+                        />
+                    </div>
+
+                    <div className="flex flex-col gap-2">
+                        <label className="text-xs font-bold uppercase tracking-tighter">ID du producteur</label>
+                        <input
+                            type="text"
+                            placeholder="entrez l'identifiant du producteur'"
+                            value={idproducteur}
+                            onChange={(e) => setIdProducteur(e.target.value)}
+                            className="border border-zinc-950 bg-transparent px-4 py-2 outline-none focus:bg-zinc-200 transition-colors placeholder:text-zinc-500"
+                            required
                         />
                     </div>
 
@@ -134,9 +169,9 @@ export default function UpdateProducteur({ params }: { params: Promise<{ id: str
                             type="submit" 
                             className="bg-zinc-950 text-zinc-50 py-3 font-medium hover:bg-zinc-800 transition-all uppercase tracking-widest text-sm"
                         >
-                            Modifier le producteur
+                            Modifier le produit
                         </button>
-                    </div>
+                    </div>   
                 </form>
 
                 <form onSubmit={deletionSubmit} className="flex flex-col gap-6">
@@ -145,18 +180,21 @@ export default function UpdateProducteur({ params }: { params: Promise<{ id: str
                             type="submit"
                             className="bg-red-700 text-white py-3 font-medium hover:bg-red-800 transition-all uppercase tracking-widest text-sm shadow-sm"
                         >
-                            Supprimer le producteur
+                            Supprimer le produit
                         </button>
                     </div>
-                </form>
-
+                </form>     
+                        
                 <Link 
                     href="/admin/dashboard" 
                     className="text-center text-xs text-zinc-600 hover:text-zinc-950 underline underline-offset-4 transition-colors"
                 >
                     Revenir au dashboard
                 </Link>
+            
             </div>
         </div>
-    )
+    );
+
+
 }
--- app/api/admin/produits/[id]/route.ts
+++ app/api/admin/produits/[id]/route.ts
@@ -1,11 +1,12 @@
 import { NextResponse } from 'next/server';
 import { prisma } from '@/lib/prisma';
 
-export async function PATCH(request: Request, { params }: { params: { id: string } }) {
+export async function PATCH(request: Request, { params }: { params: Promise<{ id: string }> }) {
     try {
-        const id = Number.parseInt(params.id);
-        const body = await request.json();
+        const { id: idStr } = await params;
+        const id = Number.parseInt(idStr);
 
+        const body = await request.json();
         const { nom, description, type, lienBoutique, producteurId } = body;
 
         const updated = await prisma.produit.update({
@@ -20,28 +21,41 @@ export async function PATCH(request: Request, { params }: { params: { id: string
         });
 
         return NextResponse.json(updated);
-    } catch {
-        return NextResponse.json(
-            { error: "Échec de la mise à jour" },
-            { status: 500 }
-        );
+    } catch (error) {
+        console.error("Erreur PATCH Produit:", error);
+        return NextResponse.json({ error: "Échec de la mise à jour" },{ status: 500 });
     }
 }
 
-
-export async function DELETE(request: Request, { params }: { params: { id: string } }) {
+export async function DELETE(request: Request, { params }: { params: Promise<{ id: string }> }) {
     try {
-        const id = Number.parseInt(params.id);
+        const { id: idStr } = await params;
+        const id = Number.parseInt(idStr);
 
         await prisma.produit.delete({
             where: { id }
         });
 
         return NextResponse.json({ message: "Produit supprimé" });
-    } catch {
-        return NextResponse.json(
-            { error: "Erreur lors de la suppression" },
-            { status: 500 }
-        );
+    } catch (error) {
+        console.error("Erreur DELETE Produit:", error);
+        return NextResponse.json({ error: "Erreur lors de la suppression" },{ status: 500 });
+    }
+}
+
+export async function GET(request: Request, { params }: { params: Promise<{ id: string }> }) {
+    try {
+        const resolvedParams = await params;
+        const id = Number(resolvedParams.id);
+
+        const produit = await prisma.produit.findUnique({
+            where: { id: id },
+        });
+
+        if (!produit) return NextResponse.json({ message: "Non trouvé" }, { status: 404 });
+
+        return NextResponse.json(produit);
+    } catch (error) {
+        return NextResponse.json({ message: "Erreur serveur" }, { status: 500 });
     }
 }
